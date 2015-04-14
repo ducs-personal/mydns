8ce87d626195f5d002c3f79473df654411dc16bc
ducs <duchengshuai@zmeng123.com>
2015-04-15 07:30:08 +0800

    rewrite

--- zdns.c
+++ zdns.c
@@ -13,18 +13,6 @@
 #include<netinet/in.h>
 #include<unistd.h>
 
-#define T_A 1          //Ipv4 address
-#define T_NS 2         //Nameserver
-#define T_CNAME 5      // canonical name
-#define T_SOA 6        /* start of authority zone */
-#define T_PTR 12       /* domain name pointer */
-#define T_MX 15        //Mail server
-
-void zgethostbyname(unsigned char*, int);
-void domain_format(unsigned char*, unsigned char*);
-unsigned char* read_name(unsigned char*, unsigned char*, int*);
-void get_dns_servers();
-
 typedef struct _t_dns_header
 {
     unsigned short id;          // identification number
@@ -80,8 +68,40 @@ typedef struct _t_query
     question_t *ques;
 } query_t;
 
-static char dns_servers[10][32];
-res_t answers[20], auth[20], addit[20];          //the replies from the DNS server
+#define T_A 1          //Ipv4 address
+#define T_NS 2         //Nameserver
+#define T_CNAME 5      // canonical name
+#define T_SOA 6        /* start of authority zone */
+#define T_PTR 12       /* domain name pointer */
+#define T_MX 15        //Mail server
+
+static char dns_servers[2][32];
+static res_t answers[20];
+//static char ipstr[32];
+
+void get_dns_servers(char *);
+void zgethostbyname(char*, char *, int);
+void domain_format(unsigned char*, unsigned char*);
+char* read_name(unsigned char*, unsigned char*, int*);
+
+int isready(int fd)
+{
+    int res;
+    fd_set rfds;
+    struct timeval tv;
+    tv.tv_sec = 60;
+    tv.tv_usec = 0;
+
+    FD_ZERO(&rfds);
+    FD_SET(fd, &rfds);
+    res = select(fd + 1, &rfds, NULL, NULL, &tv);
+    if (res < 0)
+    {
+
+        return -1;
+    }
+    return FD_ISSET(fd, &rfds) ? 1 : 0;
+}
 
 void _show_dnshdr(dns_header_t *dnshdr)
 {
@@ -91,7 +111,7 @@ void _show_dnshdr(dns_header_t *dnshdr)
     printf("\n %d Authoritative Servers.", ntohs(dnshdr->auth_count));
     printf("\n %d Additional records.\n\n", ntohs(dnshdr->add_count));
 }
-void _show_answers(int ans_count, int auth_count, int add_count)
+void _show_answers(int ans_count, char *ipstr)
 {
     int i = 0;
     struct sockaddr_in a;
@@ -107,6 +127,8 @@ void _show_answers(int ans_count, int auth_count, int add_count)
             p = (long*) answers[i].rdata;
             a.sin_addr.s_addr = (*p);          //working without ntohl
             printf("has IPv4 address : %s", inet_ntoa(a.sin_addr));
+            strcpy(ipstr, inet_ntoa(a.sin_addr));
+            break;
         }
 
         if (ntohs(answers[i].resource->type) == 5)
@@ -118,58 +140,17 @@ void _show_answers(int ans_count, int auth_count, int add_count)
         printf("\n");
     }
 
-    // print authorities
-    printf("\nAuthoritive Records : %d \n", auth_count);
-    for (i = 0; i < auth_count; i++)
-    {
-
-        printf("Name : %s ", auth[i].name);
-        if (ntohs(auth[i].resource->type) == 2)
-        {
-            printf("has nameserver : %s", auth[i].rdata);
-        }
-        printf("\n");
-    }
-
-    //print additional resource records
-    printf("\nAdditional Records : %d \n", add_count);
-    for (i = 0; i < add_count; i++)
-    {
-        printf("Name : %s ", addit[i].name);
-        if (ntohs(addit[i].resource->type) == 1)
-        {
-            long *p;
-            p = (long*) addit[i].rdata;
-            a.sin_addr.s_addr = (*p);
-            printf("has IPv4 address : %s", inet_ntoa(a.sin_addr));
-        }
-        printf("\n");
-    }
-    return;
 }
-void zgethostbyname(unsigned char *host, int query_type)
+
+void _pack_sockaddr(struct sockaddr_in *dest)
 {
-    unsigned char buf[65536], *qname, *reader;
-    int i, j, stop;
+    dest->sin_family = AF_INET;
+    dest->sin_port = htons(53);
+    dest->sin_addr.s_addr = inet_addr(dns_servers[0]);          //dns servers
+}
 
-    int sock = -1;
-    struct sockaddr_in a;
-    struct sockaddr_in dest;
-
-    dns_header_t *dnshdr = NULL;
-    question_t *qinfo = NULL;
-
-    printf("Resolving %s", host);
-
-    sock = socket(AF_INET, SOCK_DGRAM, IPPROTO_UDP);          //UDP packet for DNS queries
-
-    dest.sin_family = AF_INET;
-    dest.sin_port = htons(53);
-    dest.sin_addr.s_addr = inet_addr(dns_servers[0]);          //dns servers
-
-    //Set the DNS structure to standard queries
-    dnshdr = (dns_header_t *) &buf;
-
+void _pack_dnshdr(dns_header_t *dnshdr)
+{
     dnshdr->id = (unsigned short) htons(getpid());
     dnshdr->qr = 0;          //This is a query
     dnshdr->opcode = 0;          //This is a standard query
@@ -185,40 +166,18 @@ void zgethostbyname(unsigned char *host, int query_type)
     dnshdr->ans_count = 0;
     dnshdr->auth_count = 0;
     dnshdr->add_count = 0;
+}
 
-    qname = (unsigned char*) &buf[sizeof(dns_header_t)];
-
-    domain_format(qname, host);
-    qinfo = (question_t*) &buf[sizeof(dns_header_t) + (strlen((const char*) qname) + 1)];          //fill it
-
-    qinfo->qtype = htons(query_type);          //type of the query , A , MX , CNAME , NS etc
-    qinfo->qclass = htons(1);          //its internet (lol)
-
-    printf("\nSending Packet...");
-    if (sendto(sock, (char*) buf, sizeof(dns_header_t) + (strlen((const char*) qname) + 1) + sizeof(question_t), 0, (struct sockaddr*) &dest, sizeof(dest))
-            < 0)
-    {
-        perror("sendto failed");
-    }
-    printf("Done");
-
-    //Receive the answer
-    int len = -1;
-    len = sizeof(dest);
-    printf("\nReceiving answer...");
-    if (recvfrom(sock, (char*) buf, 65536, 0, (struct sockaddr*) &dest, (socklen_t*) &(len)) < 0)
-    {
-        perror("recvfrom failed");
-    }
-    printf("Done");
-
+void _deal_recv(char *buf, int qname_len)
+{
+    dns_header_t * dnshdr = NULL;
+    int i = 0, j = 0, stop = 0;
+    char *reader = NULL, qname = NULL;
     dnshdr = (dns_header_t*) buf;
     _show_dnshdr(dnshdr);
 
-    reader = &buf[sizeof(dns_header_t) + (strlen((const char*) qname) + 1) + sizeof(question_t)];
+    reader = (buf + sizeof(dns_header_t) + qname_len + sizeof(question_t));
     //Start reading answers
-    stop = 0;
-
     for (i = 0; i < ntohs(dnshdr->ans_count); i++)
     {
         answers[i].name = read_name(reader, buf, &stop);
@@ -237,7 +196,6 @@ void zgethostbyname(unsigned char *host, int query_type)
             }
 
             answers[i].rdata[ntohs(answers[i].resource->data_len)] = '\0';
-
             reader = reader + ntohs(answers[i].resource->data_len);
         }
         else
@@ -246,49 +204,58 @@ void zgethostbyname(unsigned char *host, int query_type)
             reader = reader + stop;
         }
     }
-
-    //read authorities
-    for (i = 0; i < ntohs(dnshdr->auth_count); i++)
-    {
-        auth[i].name = read_name(reader, buf, &stop);
-        reader += stop;
-
-        auth[i].resource = (res_data_t*) (reader);
-        reader += sizeof(res_data_t);
-
-        auth[i].rdata = read_name(reader, buf, &stop);
-        reader += stop;
-    }
-
-    //read additional
-    for (i = 0; i < ntohs(dnshdr->add_count); i++)
-    {
-        addit[i].name = read_name(reader, buf, &stop);
-        reader += stop;
-
-        addit[i].resource = (res_data_t*) (reader);
-        reader += sizeof(res_data_t);
-
-        if (ntohs(addit[i].resource->type) == 1)
-        {
-            addit[i].rdata = (unsigned char*) malloc(ntohs(addit[i].resource->data_len));
-            for (j = 0; j < ntohs(addit[i].resource->data_len); j++)
-                addit[i].rdata[j] = reader[j];
-
-            addit[i].rdata[ntohs(addit[i].resource->data_len)] = '\0';
-            reader += ntohs(addit[i].resource->data_len);
-        }
-        else
-        {
-            addit[i].rdata = read_name(reader, buf, &stop);
-            reader += stop;
-        }
-    }
-    _show_answers(ntohs(dnshdr->ans_count), ntohs(dnshdr->auth_count), ntohs(dnshdr->add_count));
-
 }
 
-u_char* read_name(unsigned char* reader, unsigned char* buffer, int* count)
+void zgethostbyname(char *host, char *ipstr, int query_type)
+{
+    int sock = -1;
+    int len = -1;
+    int qname_len = 0;
+    unsigned char *qname = NULL;
+    unsigned char *buf = NULL;
+    question_t *qinfo = NULL;
+
+    struct sockaddr_in dest;
+
+    printf("Resolving %s", host);
+    buf = malloc(1024);
+
+    sock = socket(AF_INET, SOCK_DGRAM, IPPROTO_UDP);          //UDP packet for DNS queries
+    _pack_sockaddr(&dest);
+
+    _pack_dnshdr((dns_header_t *) buf);
+
+    qname = (unsigned char*) (buf + sizeof(dns_header_t));
+    domain_format(qname, host);
+    qname_len = (strlen((const char*) qname) + 1);
+
+    qinfo = (question_t*) (buf + sizeof(dns_header_t) + qname_len);          //fill it
+    qinfo->qtype = htons(query_type);          //type of the query , A , MX , CNAME , NS etc
+    qinfo->qclass = htons(1);          //its internet (lol)
+
+    printf("\nSending Packet...");
+    if (sendto(sock, (char*) buf, sizeof(dns_header_t) + qname_len + sizeof(question_t), 0, (struct sockaddr*) &dest, sizeof(dest)) < 0)
+    {
+        perror("sendto failed");
+    }
+    printf("Done");
+
+    //Receive the answer
+    len = sizeof(dest);
+    printf("\nReceiving answer...");
+    if (recvfrom(sock, (char*) buf, 1024, 0, (struct sockaddr*) &dest, (socklen_t*) &(len)) < 0)
+    {
+        perror("recvfrom failed");
+    }
+    printf("Done");
+
+    _deal_recv(buf, qname_len);
+    _show_answers(ntohs(((dns_header_t *) buf)->ans_count), ipstr);
+
+    free(buf);
+}
+
+char* read_name(unsigned char* reader, unsigned char* buffer, int* count)
 {
     unsigned char *name;
     unsigned int p = 0, jumped = 0, offset;
@@ -296,17 +263,16 @@ u_char* read_name(unsigned char* reader, unsigned char* buffer, int* count)
 
     *count = 1;
     name = (unsigned char*) malloc(256);
-
     name[0] = '\0';
 
     //read the names in 3www6google3com format
     while (*reader != 0)
     {
         if (*reader >= 192)
-        {
-            offset = (*reader) * 256 + *(reader + 1) - 49152;          //49152 = 11000000 00000000 ;)
+        {          //192=11000000
+            offset = ((*reader) << 8) + *(reader + 1) - 49152;          //49152 = 11000000 00000000 ;)
             reader = buffer + offset - 1;
-            jumped = 1;          //we have jumped to another location so counting wont go up!
+            jumped = 1;          // jumped to another location so counting wont go up!
         }
         else
         {
@@ -321,7 +287,7 @@ u_char* read_name(unsigned char* reader, unsigned char* buffer, int* count)
         }
     }
 
-    name[p] = '\0';          //string complete
+    name[p] = '\0';          //string done
     if (jumped == 1)
     {
         *count = *count + 1;          //number of steps we actually moved forward in the packet
@@ -338,7 +304,7 @@ u_char* read_name(unsigned char* reader, unsigned char* buffer, int* count)
         }
         name[i] = '.';
     }
-    name[i - 1] = '\0';          //remove the last dot
+    name[i - 1] = '\0';
     return name;
 }
 
@@ -355,9 +321,11 @@ void get_dns_servers(char *zdns_server)
     strcpy(dns_servers[1], "114.114.114.114");
 }
 
-void domain_format(unsigned char* dns, unsigned char* host)
+void domain_format(unsigned char* dns, unsigned char* hostp)
 {
     int lock = 0, i;
+    char host[32];
+    strcpy(host, hostp);
     strcat((char*) host, ".");
 
     for (i = 0; i < strlen((char*) host); i++)
@@ -369,27 +337,28 @@ void domain_format(unsigned char* dns, unsigned char* host)
             {
                 *dns++ = host[lock];
             }
-            lock++;          //or lock=i+1;
+            lock++;
         }
     }
     *dns++ = '\0';
 }
 
-int main(int argc, char **argv)
-{
-    if (argc < 2)
-    {
-        printf("usage.\n");
-    }
-
-    get_dns_servers(NULL);
-
-    if (argv[1])
-    {
-        zgethostbyname(argv[1], T_A);
-    }
-    return 0;
-}
+//int main(int argc, char **argv)
+//{
+//    if (argc < 2)
+//    {
+//        printf("usage.\n");
+//    }
+//
+//    get_dns_servers(NULL);
+//
+//    if (argv[1])
+//    {
+//        zgethostbyname(argv[1], T_A);
+//        printf("\n%s\n", ipstr);
+//    }
+//    return 0;
+//}
 
 /*
  * ipstr domain alloced by user
@@ -397,7 +366,24 @@ int main(int argc, char **argv)
 int zdns(char *domain, char *ipstr, int timeout)
 {
     get_dns_servers(NULL);
-    zgethostbyname(domain, T_A);
+    zgethostbyname(domain, ipstr, T_A);
+    return 0;
+}
+
+int zdns_srv(char *domain, char *ipstr, char *dns_srv, int timeout)
+{
+    get_dns_servers(dns_srv);
+    zgethostbyname(domain, ipstr, T_A);
+    return 0;
+}
+
+int main()
+{
+    char ipstr[32];
+    zdns("www.tmall.com", ipstr, 3);
+
+    puts("\n");
+    puts(ipstr);
     return 0;
 }
 
