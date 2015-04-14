c1868bd52bfa2310f83a5adab339577f31763f4a
ducs <duchengshuai@zmeng123.com>
2015-04-15 04:13:34 +0800

    origin

---
+++ zdns.h
@@ -0,0 +1,13 @@
+/*
+ * zdns.h
+ *
+ *  Created on: Apr 15, 2015
+ *      Author: root
+ */
+
+#ifndef ZDNS_ZDNS_H_
+#define ZDNS_ZDNS_H_
+
+int zdns(char *domain, char *ipstr, int timeout);
+
+#endif /* ZDNS_ZDNS_H_ */
